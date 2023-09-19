5d83d828eb71939a4e4b3aef14cf26ec5f0813ed
Goodnessmbakara <amicablembakara50@gmail.com>
2023-09-19 23:37:04 +0100

    server runs perfectly at this state

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,7 @@ import uuid
 
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=250, unique=True)
     email = models.EmailField(unique=True)
@@ -14,12 +14,15 @@ class User(AbstractBaseUser):
     access_token = models.CharField(max_length=255)
     refresh_token = models.CharField(max_length=255)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     def __str__(self):
         return self.username
 
 
 # Interested Events Model
-class interestedEvent(models.Model):
+class InterestedEvent(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey("Event", on_delete=models.CASCADE)
 
@@ -28,7 +31,6 @@ class interestedEvent(models.Model):
         return self.event.name
 
 # Images Model
-# 'Pillow' module will need to be installed for this to work.
 class Image(models.Model):
     image_id = models.AutoField(primary_key=True)
     comment = models.ForeignKey("comment", on_delete=models.CASCADE)
@@ -41,10 +43,9 @@ class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     body = models.TextField(max_length=1024)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    """comment on event fields to be removed once event model is available"""
     event = models.ForeignKey("Event", on_delete=models.DO_NOTHING)
     def __str__(self):
-        return self.username  
+        return self.username
 
 
 
@@ -53,47 +54,37 @@ class Group(models.Model):
     group_id = models.AutoField(primary_key=True)
     group_name = models.CharField(max_length=20 )
     description= models.CharField(max_length=100)
-    owner_id= models.ForeignKey(User.user_id, on_delete=models.DO_NOTHING)
+    owner_id= models.ForeignKey("User", on_delete=models.DO_NOTHING)
     created_at= models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.group_name
 
 
-class Event(models.Model):
-    event_id = models.AutoField(primary_key=True)
-    event_name= models.CharField(max_length=50)
-    start_date= models.DateField(auto_now_add=True)
-    end_date= models.DateField()
-    locaion= models.CharField(max_length=100)
-    description= models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.event_name
-
 class User_group(models.Model):
-    user= models.ForeignKey("User")
-    event= models.ForeignKey("Event")
+    """model for the user group object"""
+    user= models.ForeignKey("User",on_delete=models.DO_NOTHING)
+    event= models.ForeignKey("Event", on_delete=models.DO_NOTHING)
 
 
 
 class Event(models.Model):
     """Model representing an event."""
-    id = models.AutoField(primary_key=True, max_length=60)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=60)
     description = models.CharField(max_length=1024)
-    creator_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    creator_id = models.ForeignKey('User', on_delete=models.CASCADE)
     location = models.CharField(max_length=1024)
     start_date = models.DateField()
     end_date = models.DateField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    thumbnail=models.ImageField(blank=True,upload_to='events/') 
+    thumbnail=models.ImageField(blank=True,upload_to='events/')
 
     class Meta:
         verbose_name ='Event'
         verbose_name_plural = ("Events")
-    
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
