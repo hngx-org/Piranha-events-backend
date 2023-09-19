b9583a79f33838737b4090baac9759c0c8e88119
Conradgabe <gisuekebho5880@gmail.com>
2023-09-19 17:58:27 +0100

    integrating user model to project

--- piranhaevents/settings.py
+++ piranhaevents/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "myapp",
+    
+    # local app
+    "myapp.apps.MyappConfig",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTH_USER_MODEL = "myapp.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
