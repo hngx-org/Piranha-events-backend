b1dec51baa45b72117ac097785e8224ca8e91e38
Sidney Anya <66220080+SidKay@users.noreply.github.com>
2023-09-19 13:46:02 -0700

    Update Image and Interested events

--- myapp/models.py
+++ myapp/models.py
@@ -17,21 +17,19 @@ class User(AbstractBaseUser):
 # Interested Events Model
 class interestedEvent(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # Should be uncommented when event model is created.
-    # event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
-        # This should display the event's name (Probably).
+        # Doubt this will work. Test to see.
         return self.event.name
 
 # Images Model
 # 'Pillow' module will need to be installed for this to work.
 class Image(models.Model):
     image_id = models.AutoField(primary_key=True)
-    # Should be uncommented when comment model exists.
-    # comment = models.ForeignKey(comment, on_delete=models.CASCADE)
+    comment = models.ForeignKey(comment, on_delete=models.CASCADE)
     image = models.ImageField()
 
-    def __str__(self):
-        # I don't really know what should be returned.
-        return self.image_id
+    # def __str__(self):
+    #     # I don't really know what should be returned.
+    #     return self.image_id
