83358fb9cffa60a573876d109fb3937e5d90f9d9
Oluwatobiloba <iamtobihunkuten@gmail.com>
2023-09-19 14:39:18 +0100

    user_model
    updated user model

--- myapp/models.py
+++ myapp/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-
+import uuid
 # Create your models here.
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=250, unique=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
-    avatar = models.CharField(max_length=255)
-    oauth_token = models.CharField(max_length=255)
+    avatar = models.CharField(max_length=255, blank=True, null=True)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.username
+        return self.name
