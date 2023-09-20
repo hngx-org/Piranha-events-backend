09bb90d83d27232c8a9aea2163f68c82c2595e8d
lilywd <ndungulilianwanjiku@gmail.com>
2023-09-21 00:45:35 +0300

    events endpoints refactor based on the current schema

--- myapp/views.py
+++ myapp/views.py
@@ -10,53 +10,17 @@ from .models import User, Event, Comment, InterestedEvent,User_group
 
 
 from .serializers import CommentSerializer, ImageSerializer
-from .serializers import GroupSerializer, User_groupSerializer
+from .serializers import GroupSerializer, User_groupSerializer,InterestedEventSerializer
 
 from .models import Image
 
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
-"""This view returns a list of all events."""
-@api_view(['GET'])
-def eventList (request): 
-    events = Event.objects.all()
-    serializer = EventSerializer(events, many=True) 
-    return Response (serializer.data)
-
-
-"""This view returns the details of a specific event."""
-@api_view(['GET'])
-def eventDetail(request, pk):
-    events = Event.objects.get(id=pk)
-    serializer = EventSerializer(events, many=False) 
-    return Response (serializer.data)
-
-
-"""This view creates a new event. """
-@api_view(['POST'])
-def eventCreate(request) :
-    serializer = EventSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response (serializer.data)
-
-"""This view updates an event. """
-@api_view(['PUT'])
-def eventUpdate(request, pk):
-    event = Event.objects.get(id=pk)
-    serializer = EventSerializer (instance=event, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-"""This view deletes an event. """
-@api_view(['DELETE'])
-def eventDelete (request, pk):
-    event = Event.objects.get(id=pk) 
-    event.delete()
-    return Response ('Deleted')
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 
 @api_view(['POST'])
