c3aaa1fd3c38225a2974c769684625081a1d185d
Prideland-Okoi <prideland.okoi@gmail.com>
2023-09-20 16:08:27 +0100

    Update urls.py
    create url for group list and group details

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # User Management
+    # Event Management
     path('events/', eventList, name='event-list'),
     path('events/<int:pk>/', eventDetail, name='event-detail'),
     path('events/create/', eventCreate, name='event-create'),
@@ -11,4 +13,8 @@ urlpatterns = [
     path('api/events/<int:eventId>/comments/', get_comments, name='get-comments'),
     path('api/comments/<int:commentId>/images/', add_image_to_comment, name='add-image-to-comment'),
     path('api/comments/<int:commentId>/images/', get_images_for_comment, name='get-images-for-comment'),
+
+    # User Interaction
+    path('api/groups/', views.groupListCreate, name='group-list-create'),
+    path('api/groups/<int:pk>/', views.groupDetail, name='group-detail'),
 ]
