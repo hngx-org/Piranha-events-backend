270dd83b5551374482c4d21b37ba33b3b0411e24
Goodnessmbakara <amicablembakara50@gmail.com>
2023-09-20 12:42:25 +0100

    modified swagger docs

--- piranhaevents/urls.py
+++ piranhaevents/urls.py
@@ -14,16 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from . import docs
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Piranha Event API Documentation",
+        default_version='v1',
+        description="Piranha Event API Documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include('myapp.urls')),
-    path('docs/', include(docs))
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
