6324d48b6da60e4ce03e6430bc9657bec868b638
Prideland-Okoi <prideland.okoi@gmail.com>
2023-09-20 16:03:18 +0100

    Update serializers.py
    created serializer for Group

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from . models import Event
+from . models import Event, Group
 
 class EventSerializer(serializers.ModelSerializer):
     """EventSerializer class converts Event objects to and from JSON."""
     class Meta:
         model = Event
+        fields = '__all__'
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
         fields = '__all__'
