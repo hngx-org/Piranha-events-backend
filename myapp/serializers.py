ce94a8fb0d8ebcc9ae71e1ee1c475d3d9fcb9923
Frankie Vexx <franblog@gmail.com>
2023-09-20 16:34:25 +0300

    Created serilaizer class for the comments table/views

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from . models import Event
+from . models import Event, Comment
 
 class EventSerializer(serializers.ModelSerializer):
     """EventSerializer class converts Event objects to and from JSON."""
     class Meta:
         model = Event
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    """This is a class serilializer, it converts comment objects to and from json"""
+    class Meta:
+        model = Comment
         fields = '__all__'
