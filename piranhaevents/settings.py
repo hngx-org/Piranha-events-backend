da59f20da93d32e62fb80f77ec7572a19cfe0cb5
Chiemelie10 <chiemelieeze@yahoo.com>
2023-09-21 16:20:22 +0100

    google auth branch

--- piranhaevents/settings.py
+++ piranhaevents/settings.py
@@ -188,7 +188,7 @@ SOCIALACCOUNT_PROVIDERS = {
     'google': {
         # For each OAuth based provider, either add a ``SocialApp``
         # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
+        # credentials, or list them here.:
         'SCOPE': [
             'profile',
             'email',
