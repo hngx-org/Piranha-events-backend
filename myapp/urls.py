5636b499793d3b2b48412e7f12f345c96c623f0b
Goodnessmbakara <amicablembakara50@gmail.com>
2023-09-21 12:08:37 +0100

    complete structure and list of endpoints to implemennt just left the user and auh endpoints

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,9 @@ router.register(r'events', EventViewSet)
 
 
 urlpatterns = [
-    
+    path('users/<int:pk>', userGet, name='user-get'),
+    path('users/<int:pk>/update', userUpdate, name='user-update'),
+
     # Event Management
     path('', include(router.urls)),
   
@@ -23,14 +25,18 @@ urlpatterns = [
     # Remove interest in an event
     path('users/<int:userId>/interests/<int:eventId>/', remove_interest, name='remove-interest'),
 
+    path('groups/',create_goup, name='group-create'),
+    path('groups/<int:groupId>/', get_specific_group, name='group-detail'),
+    path('groups/<int:groupId>/', update_group, name='group-update'),
+    path('groups/<int:groupId>/', delete_group, name='group-delete'),
     path('groups/', get_groups, name='group-list'),
-    path('groups/create',create_goup, name='group-create'),
-    path('groups/<int:groupId>', get_specific_group, name='group-detail'),
-    path('groups/<int:groupId>/update', update_group, name='group-update'),
-    path('groups/<int:groupId>/delete', delete_group, name='group-delete'),
 
-    path('users/<int:pk>', userGet, name='user-get'),
-    path('users/<int:pk>/update', userUpdate, name='user-update'),
-
+    "path('groups/<int:groupId>/members/<int:userId>', add_user_to_group, name='group-user-create'),"
+    "path('groups/<int:groupId>/members/<int:userId>', remove_user_from_group, name='group-user-delete'),"
+    "path('groups/<int:groupId>/members/', group_members_list, name='group-members-list'),"
+    
+    "path('comments/<int:commentId>/likes/', add_likes_to_comment, name='add-likes-to-comment'),"
+    "path('comments/<int:commentId>/likes/', get_likes_for_comment, name='get-likes-for-comment'),"
+    "path('comments/<int:commentId>/likes/', delete_likes_for_comment, name='delete-likes-for-comment'),"
 ]
 
