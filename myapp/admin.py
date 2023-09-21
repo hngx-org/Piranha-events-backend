62e1892214a972b01fb6824713e2d4e627663917
dju-tech <116469507+dju-tech@users.noreply.github.com>
2023-09-21 15:07:37 +0100

    Update admin.py
    fixed error <class 'myapp.admin.CustomUserAdmin'>: (admin.E108) The value of 'list_display[0]' refers to 'username', which is not a callable, an attribute of 'CustomUserAdmin'

--- myapp/admin.py
+++ myapp/admin.py
@@ -15,11 +15,11 @@ class CustomUserAdmin(UserAdmin):
 
 	model = User
 
-	list_display = ('username', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login',)
+	list_display = ('name', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login',)
 	list_filter = ('is_active', 'is_staff', 'is_superuser')
 
 	fieldsets = (
-		(None, {'fields' : ('username', 'email', 'password')}),
+		(None, {'fields' : ('name', 'email', 'password')}),
 		('Permissions', {'fields' : ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
 		('Dates', {'fields' : ('last_login', 'date_joined')})
 	)
@@ -27,7 +27,7 @@ class CustomUserAdmin(UserAdmin):
 		(None,{
 
 			'classes' : ('wide',),
-			'fields' : ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active')
+			'fields' : ('name', 'email', 'password1', 'password2', 'is_staff', 'is_active')
 		}
 
 		),
