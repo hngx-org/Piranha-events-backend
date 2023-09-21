9fe6c1594ef9c0e0b73a762f51a805faf01b517f
Goodnessmbakara <amicablembakara50@gmail.com>
2023-09-21 21:01:33 +0100

    some fixes

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-import uuid
+
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
